5776bdd31b7c56443d705443fbe89d5654fef01e
cdparra <cdparra@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-09 00:03:23 +0000

    Clase Action para Especialidad.

---
+++ appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/EspecialidadAction.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.fpuna.webapp.action;
+
+import edu.fpuna.model.Especialidad;
+import edu.fpuna.service.EspecialidadManager;
+import java.util.List;
+
+/**
+ *
+ * @author Cristhian Parra
+ */
+public class EspecialidadAction extends BaseAction{
+
+    private EspecialidadManager manager;
+    List<Especialidad> especialidades;
+    
+    public Especialidad getEspecialidad(String nombre) {
+        return manager.getEspecialidad(nombre);
+    }
+
+    public void setEspecialidadManager(EspecialidadManager especialidadManager) {
+        this.manager = especialidadManager;
+    }
+
+    public String list() {
+        especialidades = manager.getAll();
+        return SUCCESS;
+    }
+}
