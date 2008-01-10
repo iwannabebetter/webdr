e19aeffae25c33620eba5384cd38c5fba252e490
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-10 23:28:47 +0000

    Clase que representa un dia de la semana, para se utilizado en la clase HorarioAtencion.

---
+++ trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/model/DiaDeSemana.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.fpuna.model;
+
+/**
+ * Clase que representa un dia de la semana
+ * @author ghuttemann
+ */
+public enum DiaDeSemana {
+    DOMINGO,
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO
+}
