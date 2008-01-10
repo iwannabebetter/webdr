f0fbb464b3e0e865b81817bda587d103795575c3
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-10 03:13:21 +0000

    Se agrega la clase Consulta. Los datos de medidas del paciente y las notas de la consulta se anotan por separado, en las clases MedidasPaciente y Notas, respectivamente, a pesar de que todo se almacena en la misma tabla.

---
+++ appfuse-projects/webdr/src/main/java/edu/fpuna/model/Consulta.java
@@ -0,0 +1,129 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.fpuna.model;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import javax.persistence.*;
+
+/**
+ *
+ * @author ghuttemann
+ */
+@Entity
+@Table(name="consulta")
+public class Consulta extends BaseObject {
+
+    private Long id;
+    private Date fecha;
+    private Timestamp horaInicio;
+    private Timestamp horaFin;
+    private MedidasPaciente medidasPaciente;
+    private Notas notas;
+    private Doctor doctor;
+    private Paciente paciente;
+
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name="fecha",nullable=false)
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    @Column(name="hora_inicio",nullable=false)
+    public Timestamp getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(Timestamp horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    @Column(name="hora_fin",nullable=false)
+    public Timestamp getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(Timestamp horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    @Embedded
+    public MedidasPaciente getMedidasPaciente() {
+        return medidasPaciente;
+    }
+
+    public void setMedidasPaciente(MedidasPaciente medidasPaciente) {
+        this.medidasPaciente = medidasPaciente;
+    }
+    
+    @Embedded
+    public Notas getNotas() {
+        return notas;
+    }
+
+    public void setNotas(Notas notas) {
+        this.notas = notas;
+    }
+    
+    @OneToOne(fetch=FetchType.LAZY,optional=false)
+    @JoinColumn(name="doctor_id",nullable=false)
+    public Doctor getDoctor() {
+        return doctor;
+    }
+    
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+    
+    @OneToOne(fetch=FetchType.LAZY,optional=false)
+    @JoinColumn(name="paciente_id",nullable=false)
+    public Paciente getPaciente() {
+        return paciente;
+    }
+    
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+    
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "(id=" + this.id + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Consulta)) {
+            return false;
+        }
+
+        final Consulta consulta = (Consulta) o;
+
+        if (this.getId().equals(consulta.getId())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        if (this.getId() == null)
+            return 0;
+        
+        return this.getId().hashCode();
+    }
+}
