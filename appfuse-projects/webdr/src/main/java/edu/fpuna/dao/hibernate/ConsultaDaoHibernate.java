8a928150f6b5006df22b66b9871988f73d8ea4d8
fernandomancia <fernandomancia@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-10 22:58:28 +0000

    Cree la interface ConsultaDao.java y la implementacion ConsultaDaoHibernate.java

---
+++ appfuse-projects/webdr/src/main/java/edu/fpuna/dao/hibernate/ConsultaDaoHibernate.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.fpuna.dao.hibernate;
+
+import edu.fpuna.dao.ConsultaDao;
+import edu.fpuna.model.Consulta;
+import edu.fpuna.model.Paciente;
+import java.util.List;
+/**
+ *
+ * @author Liz
+ */
+public class ConsultaDaoHibernate 
+        extends GenericDaoHibernate<Consulta, Long> implements ConsultaDao{
+
+    public ConsultaDaoHibernate() {
+        super(Consulta.class);
+    }
+           
+    //@Override
+    public List<Consulta> obtenerConsultasPaciente(String string) {
+        return null;
+    }
+
+    //@Override
+    public List<Consulta> obtenerConsultasFecha() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    //@Override
+    public List<Consulta> obtenerConsultasDoctor(String user) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
