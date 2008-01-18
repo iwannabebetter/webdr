83fce32dfa5318700e5b73c50cc1814165855e73
rodas.marcelo <rodas.marcelo@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-18 22:21:06 +0000

    Se agregaron pruebas a ConsultaDao. Obs: La prueba de obtener consultas por Fecha, no compara bien la fecha guardada como tipo DATE (controlar), por ello da FAILURE.

--- appfuse-projects/webdr/src/test/java/edu/fpuna/dao/ConsultaDaoTest.java
+++ appfuse-projects/webdr/src/test/java/edu/fpuna/dao/ConsultaDaoTest.java
@@ -1,13 +1,15 @@
 package edu.fpuna.dao;
 
 import edu.fpuna.model.Consulta;
+import java.util.Date;
 import java.util.List;
 /**
  * Clase que realiza las pruebas sobre el DAO de las 
  * Consulta (ConsultaDao)
  * @author mrodas
  */
-public class ConsultaDaoTest extends BaseDaoTestCase{
+public class ConsultaDaoTest extends BaseDaoTestCase {
+    
     // Objeto DAO de prueba
     private ConsultaDao consultaDao = null;
     
@@ -16,26 +18,65 @@ public class ConsultaDaoTest extends BaseDaoTestCase{
     }
     
     /*
-     * Test nº1. Prueba de busqueda de Consulta por Paciente
+     * Test nº1. Prueba de busqueda de una Consulta por su id
+     */
+    public void testObtenerConsultaId() throws Exception {
+        log.debug("Testing(1) ObtenerConsultaId...");
+
+        Consulta consulta = consultaDao.obtenerConsultaId(-1);
+       
+        assertTrue(consulta.getId() == -1);
+        log.debug("Testing ObtenerConsultaId ha Finalizado.");
+    }
+    
+    /*
+     * Test nº2. Prueba de busqueda de Consulta por Paciente
      */
     public void testObtenerConsultasPaciente() throws Exception {
-        
-        log.debug("Consultando Paciente");
+        log.debug("Testing(2) ObtenerConsultasPaciente...");
+
         List<Consulta> consultas = consultaDao.obtenerConsultasPaciente("user");
-        log.debug(" Esta es la cantidad de Consultas " + consultas.size());
-        
+       
         assertTrue(consultas.isEmpty() != true);
+        log.debug("Cantidad de consultas del Paciente: " + consultas.size());
+        log.debug("Testing ObtenerConsultasPaciente ha Finalizado.");
     }
 
     /*
-     * Test nº2. Prueba de busqueda de Consulta por Doctor
+     * Test nº3. Prueba de busqueda de Consulta por Doctor
      */
-    public void testObtenerConsultasDoctor() throws Exception {
+    public void testObtenerConsultasDoctor() throws Exception { 
+        log.debug("Testing(3) ObtenerConsultasDoctor...");
         
-        log.debug("Consultando Doctor");
         List<Consulta> consultas = consultaDao.obtenerConsultasDoctor("admin");
-        log.debug(" Esta es la cantidad de Consultas " + consultas.size());
         
         assertTrue(consultas.isEmpty() != true);
+        log.debug("Cantidad de consultas del Doctor: " + consultas.size());
+        log.debug("Testing ObtenerConsultasDoctor ha Finalizado.");
     }
+    
+    /*
+     * Test nº4. Prueba de busqueda de Consulta por Fecha
+     */
+    public void testObtenerConsultasFecha() throws Exception { 
+        log.debug("Testing(4) ObtenerConsultasFecha...");
+        Date fecha = new Date(84, 5, 5, 0, 0, 0);
+        List<Consulta> consultas = consultaDao.obtenerConsultasFecha(fecha);
+        log.debug("Cantidad de consultas del Doctor: " + consultas.size());
+        assertTrue(consultas.isEmpty() != true);
+
+        log.debug("Testing ObtenerConsultasFecha ha Finalizado.");
+    }
+
+    /*
+     * Test nº5. Prueba de eliminar una consulta
+     */
+    public void testEliminarConsulta() throws Exception { 
+        log.debug("Testing(5) EliminarConsulta...");
+        
+        consultaDao.remove((long)-1);
+        
+        log.debug("Testing EliminarConsulta ha Finalizado.");
+    }
+  
 }
