a2c2e01e36c605c5d6457948cffeee18c60855a8
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-22 00:41:39 +0000

    Se agrega el DAO para Doctor, aún no tiene métodos.

---
+++ trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/dao/DoctorDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.fpuna.dao;
+
+import edu.fpuna.model.Doctor;
+
+/**
+ * Interfaz DAO para Doctor.
+ * @author ghuttemann
+ */
+public interface DoctorDao extends GenericDao<Doctor, Long> {
+    
+    
+}
