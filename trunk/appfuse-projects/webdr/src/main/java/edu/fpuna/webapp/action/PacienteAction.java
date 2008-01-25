c591a855524309de4696a2f3ad64180d21c07edd
fernandomancia <fernandomancia@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-25 21:07:08 +0000

    COn los manager de tipo sangre y paciente

--- trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/PacienteAction.java
+++ trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/PacienteAction.java
@@ -6,6 +6,12 @@
 package edu.fpuna.webapp.action;
 
 import edu.fpuna.model.Paciente;
+<<<<<<< .mine
+import edu.fpuna.model.TipoSangre;
+import edu.fpuna.service.EspecialidadManager;
+=======
+>>>>>>> .r173
+import edu.fpuna.service.GenericManager;
 import edu.fpuna.service.PacienteManager;
 import java.util.List;
 
@@ -15,11 +21,21 @@ import java.util.List;
  */
 public class PacienteAction extends BaseAction {
 
+    private GenericManager<TipoSangre,Long> tipoSangreManager;
+    List<TipoSangre> tipoSangres;
+    
     private PacienteManager manager;
     List<Paciente> pacientes;
 
+    private Paciente paciente;
+    private Long id;
+    
     public void setPacienteManager(PacienteManager pacienteManager) {
         this.manager = pacienteManager;
+    }
+    
+    public void setTipoSangreManager(GenericManager<TipoSangre,Long> tipoSangreManager) {
+        this.tipoSangreManager = tipoSangreManager;
     }
     
     public Paciente getPaciente(String username) {
@@ -28,6 +44,10 @@ public class PacienteAction extends BaseAction {
 
     public List<Paciente> getPacientes(){
         return pacientes;
+    }
+    
+    public List<TipoSangre> getTipoSangres(){
+        return tipoSangres;
     }
     
     public void guardar(Paciente p){
@@ -42,4 +62,52 @@ public class PacienteAction extends BaseAction {
         pacientes = manager.getAll();
         return SUCCESS;
     }
+    
+    public void  setId(Long id){
+        this.id = id;
+    }
+    
+    public Paciente getPaciente(){
+        return this.paciente;
+    }
+    
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+    
+    public String delete() {
+        manager.borrarPaciente(paciente);
+        saveMessage(getText("paciente.deleted"));
+        return SUCCESS;
+    }
+    
+    public String edit() {
+        tipoSangres = tipoSangreManager.getAll();
+        if (id != null) {
+            paciente = manager.getPaciente(id);
+        } else {
+            paciente = new Paciente();
+	}
+        return SUCCESS;
+    }
+    
+    public String save() throws Exception {
+        if (cancel != null) {
+            return "cancel";
+        }
+        if (delete != null) {
+            return delete();
+        }
+        boolean isNew = (paciente.getId() == null);
+        paciente = manager.guardarPaciente(paciente);
+        String key = (isNew) ? "paciente.added" : "paciente.updated";
+        saveMessage(getText(key));
+        if (!isNew) {
+            return INPUT;
+        } else {
+            return SUCCESS;
+        }
+    }
+    
+    
 }
