e058364f16db24b7583fc6228cb679cab327348c
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-25 14:36:35 +0000

    Se quita import innecesario.

--- appfuse-projects/webdr/src/main/java/edu/fpuna/dao/hibernate/UserDaoHibernate.java
+++ appfuse-projects/webdr/src/main/java/edu/fpuna/dao/hibernate/UserDaoHibernate.java
@@ -6,7 +6,6 @@ import org.acegisecurity.userdetails.UsernameNotFoundException;
 import edu.fpuna.dao.UserDao;
 import edu.fpuna.model.User;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.orm.hibernate3.SessionFactoryUtils;
 
