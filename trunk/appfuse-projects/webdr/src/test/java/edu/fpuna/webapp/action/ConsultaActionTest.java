62b4a0ad77a151e9b3293c8d7e39a6e2d40234d5
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-26 21:39:56 +0000

    Ligeras modificaciones.

--- trunk/appfuse-projects/webdr/src/test/java/edu/fpuna/webapp/action/ConsultaActionTest.java
+++ trunk/appfuse-projects/webdr/src/test/java/edu/fpuna/webapp/action/ConsultaActionTest.java
@@ -14,13 +14,10 @@
 package edu.fpuna.webapp.action;
 
 import com.opensymphony.xwork2.ActionSupport;
-import java.util.Date;
 import org.apache.struts2.ServletActionContext;
 import edu.fpuna.service.ConsultaManager;
 import edu.fpuna.model.Consulta;
 import edu.fpuna.model.Notas;
-import edu.fpuna.model.Doctor;
-import edu.fpuna.webapp.action.BaseActionTestCase;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
@@ -33,84 +30,80 @@ public class ConsultaActionTest extends BaseActionTestCase {
 
     @Override
     protected void onSetUpBeforeTransaction() throws Exception {
-        log.debug("Seteo antes de la transaccion");
+        log.debug("Realizando SetUp...");
         super.onSetUpBeforeTransaction();
         
-        log.debug("Recuperando el Manager");
+        log.debug("Recuperando ConsultaManager...");
         ConsultaManager consultaManager = (ConsultaManager) applicationContext.getBean("consultaManager");
         
-        log.debug("Estableciendo el Manager");
+        log.debug("Estableciendo ConsultaManager...");
         action = new ConsultaAction();
         action.setConsultaManager(consultaManager);
         
         // add a test person to the database
-//        log.debug("Agregando una especialidad");
-//        Consulta consulta = new Consulta();
-//        Date fecha  = new Date();
-//        
-//        fecha.setTime(10);
-//        
-//        consulta.setFecha(fecha);
-//        consulta.setHoraInicio(fecha);
-//        fecha.setTime(54);
-//        consulta.setHoraFin(fecha);
-//        consulta.getNotas().setSintomas("Muere");
-//        consulta.getNotas().setDiagnostico("Moriras...");
-//        
-//        
-//        especialidad.setNombre("Pediatria");
-//        especialidad.setDescripcion("Especialidad enfocada al tratamiento de niños");
-//        especialidadManager.saveEspecialidad(especialidad);
-        
+        //log.debug("Agregando una especialidad");
+        //Consulta consulta = new Consulta();
+        //Date fecha  = new Date();
+        //
+        //fecha.setTime(10);
+        //
+        //consulta.setFecha(fecha);
+        //consulta.setHoraInicio(fecha);
+        //fecha.setTime(54);
+        //consulta.setHoraFin(fecha);
+        //consulta.getNotas().setSintomas("Muere");
+        //consulta.getNotas().setDiagnostico("Moriras...");
+        //
+        //especialidad.setNombre("Pediatria");
+        //especialidad.setDescripcion("Especialidad enfocada al tratamiento de niños");
+        //especialidadManager.saveEspecialidad(especialidad);
     }
 
     public void testSearch() throws Exception {
-        log.debug("Probando el listado de consultas");
+        log.debug("Listando Consultas...");
         assertEquals(action.list(), ActionSupport.SUCCESS);
         
-        log.debug("Recuperando consultas");
         assertTrue(action.getConsultas().size() >= 1);
-        log.debug("Tamaño de consultas" + action.getConsultas().size());
+        log.debug("Cantidad de Consultas: " + action.getConsultas().size() + "...");
     }
 
-	
-	/* Pruebas Agregadas para Agregar, Editar y Borrar */
-	
-	public void testEdit() throws Exception {
-		log.debug("--> Testing edit...");
-		action.setId(1L);
-		assertNull(action.getConsulta());
-		assertEquals("success", action.edit());
-		assertNotNull(action.getConsulta());
-		assertFalse(action.hasActionErrors());
-	}
-	
-	public void testSave() throws Exception {
-		log.debug("--> Testing save...");
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		ServletActionContext.setRequest(request);
-		action.setId(1L);
-		assertEquals("success", action.edit());
-		assertNotNull(action.getConsulta());
-		// update last name and save
-		Notas notas = new Notas("01","02","03","04");
-		action.getConsulta().setNotas(notas); // VER
-		assertEquals("input", action.save());
-		assertEquals(notas, action.getConsulta().getNotas()); //VER
-		assertFalse(action.hasActionErrors());
-		assertFalse(action.hasFieldErrors());
-		assertNotNull(request.getSession().getAttribute("messages"));
-	}
+    /* Pruebas Agregadas para Agregar, Editar y Borrar */
 
-	public void testRemove() throws Exception {
-		log.debug("--> Testing remove...");
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		ServletActionContext.setRequest(request);
-		action.setDelete("");
-		Consulta consulta = new Consulta();
-		consulta.setId(2L);
-		action.setConsulta(consulta);
-		assertEquals("success", action.delete());
-		assertNotNull(request.getSession().getAttribute("messages"));
-	}
+    public void testEdit() throws Exception {
+        log.debug("Probando 'edit'...");
+        action.setId(-1L);
+        assertNull(action.getConsulta());
+        assertEquals("success", action.edit());
+        assertNotNull(action.getConsulta());
+        assertFalse(action.hasActionErrors());
+    }
+
+    public void testSave() throws Exception {
+        log.debug("Probando 'save'...");
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        ServletActionContext.setRequest(request);
+        action.setId(-1L);
+        assertEquals("success", action.edit());
+        assertNotNull(action.getConsulta());
+        // update last name and save
+        Notas notas = new Notas("01","02","03","04");
+        action.getConsulta().setNotas(notas); // VER
+        assertEquals("input", action.save());
+        assertEquals(notas, action.getConsulta().getNotas()); //VER
+        assertFalse(action.hasActionErrors());
+        assertFalse(action.hasFieldErrors());
+        assertNotNull(request.getSession().getAttribute("messages"));
+    }
+
+    public void testRemove() throws Exception {
+        log.debug("Probando 'remove'...");
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        ServletActionContext.setRequest(request);
+        action.setDelete("");
+        Consulta consulta = new Consulta();
+        consulta.setId(-2L);
+        action.setConsulta(consulta);
+        assertEquals("success", action.delete());
+        assertNotNull(request.getSession().getAttribute("messages"));
+    }
 }
