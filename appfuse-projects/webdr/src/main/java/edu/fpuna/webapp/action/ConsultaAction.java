26f76e4f4b5a9ff536a63529814f5f3322abedb0
rodas.marcelo <rodas.marcelo@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-26 00:04:11 +0000

    - Se arreglo de guardarConsulta. - Se agregaron log para el control de guardarConsula.

--- appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/ConsultaAction.java
+++ appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/ConsultaAction.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package edu.fpuna.webapp.action;
 
 import edu.fpuna.model.Consulta;
@@ -25,7 +24,6 @@ public class ConsultaAction extends BaseAction {
     List<Consulta> consultas;
     private Long id;
 
-
     public void setConsultaManager(ConsultaManager consultaManager) {
         this.manager = consultaManager;
     }
@@ -39,70 +37,89 @@ public class ConsultaAction extends BaseAction {
     }
 
     public void setConsulta(Consulta consulta) {
-	this.consulta = consulta;
+        log.debug("EJECUTANDO... "+consulta.getId());
+        this.consulta = consulta;
     }
 
     public Consulta getConsulta(Long id) {
         return manager.getConsulta(id);
     }
-    
+
     public String getConsultasPaciente(String username) {
         consultas = this.manager.getConsultasPaciente(username);
         return SUCCESS;
     }
-    
+
     public String getConsultasDoctor(String username) {
         consultas = this.manager.getConsultasDoctor(username);
         return SUCCESS;
-    }  
-    
+    }
+
     public String getConsultasFecha(Date fechaInicio, Date fechaFin) {
         consultas = this.manager.getConsultasFecha(fechaInicio, fechaFin);
         return SUCCESS;
-    } 
-    
+    }
     public List getConsultas() {
         String resultado = this.list();
         return consultas;
-    } 
-    
+    }
+
     public String list() {
         consultas = manager.getAll();
         return SUCCESS;
     }
 
     /* Delete, Editar, Guardar*/
-
     public String delete() {
         manager.remove(consulta.getId());
         saveMessage(getText("consulta.deleted"));
         return SUCCESS;
     }
-	
-    public String edit() {
-        if (id != null)
-            consulta = manager.getConsulta(id);
-        else
-            consulta = new Consulta();
 
+    public String edit() {
+        if (id != null) {
+            log.debug("EDITANDO...");
+            consulta = manager.getConsulta(id);
+            log.debug("Se obtuvo: " + consulta.getDoctor().getEmail());
+        } else {
+            consulta = new Consulta();
+        }
         return SUCCESS;
     }
-	
+
     public String save() throws Exception {
-        if (cancel != null)
-            return CANCEL;
-        
-        if (delete != null)
+        if (cancel != null) {
+            return "cancel";
+        }
+        if (delete != null) {
             return delete();
-        
+        }
         boolean isNew = (consulta.getId() == null);
-        consulta = manager.save(consulta);
+        
+        this.setId(consulta.getId());
+        
+        log.debug("GUARDANDO...");
+        log.debug("1-)Se obtuvo: " + consulta.getDoctor().getEmail());
+        /*
+         * Se recupera la consulta modificada debido a que el form (del edit)
+         * no envia los datos del Doctor y del Paciente.
+         */
+        Consulta oldConsulta = manager.getConsulta(consulta.getId());
+        log.debug("2-)Se obtuvo: " + oldConsulta.getDoctor().getEmail());
+        
+        // Se actualizan los datos del Doctor y del Paciente.
+        consulta.setDoctor(oldConsulta.getDoctor());
+        consulta.setPaciente(oldConsulta.getPaciente());
+
+        // Se guarda la consulta modificada.
+        manager.saveConsulta(consulta);
+        
         String key = (isNew) ? "consulta.added" : "consulta.updated";
         saveMessage(getText(key));
-        
-        if (!isNew)
+        if (!isNew) {
             return INPUT;
-        else
+        } else {
             return SUCCESS;
+        }
     }
 }
