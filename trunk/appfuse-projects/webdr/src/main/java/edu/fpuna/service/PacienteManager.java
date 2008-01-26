f235d84f474536be471983f5ea3e0a819813af80
cdparra <cdparra@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-26 04:08:06 +0000

    Modificaciones en la interfaz del Manager de Paciente y su implementación

--- trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/service/PacienteManager.java
+++ trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/service/PacienteManager.java
@@ -2,7 +2,9 @@ package edu.fpuna.service;
 
 import com.lowagie.text.pdf.codec.postscript.ParseException;
 import edu.fpuna.dao.PacienteDao;
+import edu.fpuna.dao.RoleDao;
 import edu.fpuna.model.Paciente;
+import edu.fpuna.model.TipoSangre;
 
 /**
  * @author fmancia
@@ -10,6 +12,10 @@ import edu.fpuna.model.Paciente;
 public interface PacienteManager extends GenericManager<Paciente,Long>{
 
     public void setPacienteDao(PacienteDao dao);
+    
+    public void setRoleDao(RoleDao roleDao);
+    
+    public void setTipoSangreManager(GenericManager<TipoSangre, Long> tipoSangreManager);
 
     public Paciente getPaciente(Long id);
 
