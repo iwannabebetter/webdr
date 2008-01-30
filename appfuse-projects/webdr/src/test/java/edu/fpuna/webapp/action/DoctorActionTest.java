8a6bd465088afb0fc8dafb7a58e6a312df7a58e2
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-30 12:20:31 +0000

    Se agregan los métodos de prueba para edit, save, delete y list(especialidad).

--- appfuse-projects/webdr/src/test/java/edu/fpuna/webapp/action/DoctorActionTest.java
+++ appfuse-projects/webdr/src/test/java/edu/fpuna/webapp/action/DoctorActionTest.java
@@ -12,6 +12,8 @@ import edu.fpuna.model.Especialidad;
 import edu.fpuna.service.DoctorManager;
 import java.sql.Date;
 import java.util.List;
+import org.apache.struts2.ServletActionContext;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * Clase de prueba del Action de Doctor.
@@ -46,6 +48,65 @@ public class DoctorActionTest extends BaseActionTestCase {
         
         log.debug("Probando cantidad de Doctores...");
         assertTrue(action.getDoctores().size() >= 1);
+    }
+    
+    public void testListPorEspecialidad() throws Exception {
+        log.debug("Probando lista de Doctores por Especialidad...");
+        
+        log.debug("\tRecuperando Especialidad...");
+        Especialidad especialidad = obtenerEspecialidad();
+        
+        log.debug("\tRecuperando Doctor...");
+        assertEquals(action.list(), DoctorAction.SUCCESS);
+        assertFalse(action.getDoctores().isEmpty());
+        Doctor doctor = action.getDoctores().get(0);
+        
+        log.debug("\tAgregando Especialidad al Doctor...");
+        doctor.agregarEspecialidad(especialidad);
+        
+        log.debug("\tListando Doctores con Especialidad '" + especialidad + "'");
+        assertEquals(action.list(especialidad), DoctorAction.SUCCESS);
+        
+        log.debug("\tProbando cantidad de Doctores recuperados...");
+        assertTrue(action.getDoctores().size() >= 1);
+    }
+    
+    public void testEdit() throws Exception {
+        log.debug("Probando edit...");
+        action.setId(-2L);
+        assertNull(action.getDoctor());
+        assertEquals("success", action.edit());
+        assertNotNull(action.getDoctor());
+        assertFalse(action.hasActionErrors());
+    }
+    
+    public void testSave() throws Exception {
+        log.debug("Probando save...");
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        ServletActionContext.setRequest(request);
+        action.setId(-2L);
+        assertEquals("success", action.edit());
+        assertNotNull(action.getDoctor());
+        
+        // update last name and save
+        action.getDoctor().setLastName("Nuevo apellido");
+        assertEquals("input", action.save());
+        assertEquals("Nuevo apellido", action.getDoctor().getLastName());
+        assertFalse(action.hasActionErrors());
+        assertFalse(action.hasFieldErrors());
+        assertNotNull(request.getSession().getAttribute("messages"));
+    }
+
+    public void testRemove() throws Exception {
+        log.debug("Probando delete...");
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        ServletActionContext.setRequest(request);
+        action.setDelete("");
+        Doctor doctor = new Doctor();
+        doctor.setId(-2L);
+        action.setDoctor(doctor);
+        assertEquals("success", action.delete());
+        assertNotNull(request.getSession().getAttribute("messages"));
     }
     
     /*
