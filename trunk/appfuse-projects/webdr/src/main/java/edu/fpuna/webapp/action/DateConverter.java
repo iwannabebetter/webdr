7cb20cb413647bcb8deb2db05035b1404789f4c7
rodas.marcelo <rodas.marcelo@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-30 17:19:45 +0000

    Se modifico el converter para el Date y el Timestamp, para que no salgan errores.

--- trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/DateConverter.java
+++ trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/DateConverter.java
@@ -3,12 +3,16 @@ package edu.fpuna.webapp.action;
 import java.text.ParseException;
 import java.util.Map;
 import java.util.Date;
+import java.sql.Timestamp;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.struts2.util.StrutsTypeConverter;
 import edu.fpuna.util.DateUtil;
 import com.opensymphony.xwork2.util.TypeConversionException;
 
 public class DateConverter extends StrutsTypeConverter {
+    private static Log log = LogFactory.getLog(DateUtil.class);
 
     public Object convertFromString(Map ctx, String[] value, Class arg2) {
         if (value[0] == null || value[0].trim().equals("")) {
@@ -16,7 +20,14 @@ public class DateConverter extends StrutsTypeConverter {
         }
 
         try {
-            return DateUtil.convertStringToDate(value[0]);
+			log.debug("Clases: "+arg2);
+			if (arg2 == Timestamp.class)
+            	return DateUtil.convertStringToDateTime(value[0]);
+			else if (arg2 == Date.class)
+            	return DateUtil.convertStringToDate(value[0]);
+			else
+				return null;
+
         } catch (ParseException pe) {
             pe.printStackTrace();
             throw new TypeConversionException(pe.getMessage());
@@ -24,6 +35,12 @@ public class DateConverter extends StrutsTypeConverter {
     }
 
     public String convertToString(Map ctx, Object data) {
-        return DateUtil.convertDateToString((Date) data);
+			log.debug("Clases: "+ data.getClass());
+			if (data.getClass() == Timestamp.class)
+            	return DateUtil.convertDateTimeToString((Date) data);
+			else if (data.getClass() == Date.class)
+            	return DateUtil.convertDateToString((Date) data);
+			else
+				return null;
     }
 } 
