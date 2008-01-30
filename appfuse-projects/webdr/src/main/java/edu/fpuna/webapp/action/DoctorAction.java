a5702487e9d925a37fdd2f38b5421541cce19abc
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-30 12:32:30 +0000

    Se agregan los métodos para edit, save, delete y list(especialidad), además de los setters y getters.

--- appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/DoctorAction.java
+++ appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/DoctorAction.java
@@ -5,6 +5,8 @@
 
 package edu.fpuna.webapp.action;
 
+import edu.fpuna.model.Doctor;
+import edu.fpuna.model.Especialidad;
 import edu.fpuna.service.DoctorManager;
 import java.util.List;
 
@@ -15,18 +17,70 @@ import java.util.List;
 public class DoctorAction extends BaseAction {
 
     private DoctorManager doctorManager;
-    private List doctores;
+    private List<Doctor> doctores;
+    private Doctor doctor;
+    private Long id;
     
     public void setDoctorManager(DoctorManager doctorManager) {
         this.doctorManager = doctorManager;
     }
     
-    public List getDoctores() {
+    public List<Doctor> getDoctores() {
         return doctores;
     }
     
     public String list() {
-        doctores = doctorManager.getAll();
+        doctores = doctorManager.obtenerDoctores();
         return SUCCESS;
+    }
+    
+    public String list(Especialidad especialidad) {
+        doctores = doctorManager.obtenerDoctoresPorEspecialidad(especialidad);
+        return SUCCESS;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public String delete() {
+        doctorManager.eliminarDoctor(doctor);
+        saveMessage(getText("doctor.deleted"));
+        return SUCCESS;
+    }
+
+    public String edit() {
+        if (id != null)
+            doctor = doctorManager.get(id);
+        else
+            doctor = new Doctor();
+
+        return SUCCESS;
+    }
+
+    public String save() throws Exception {
+        if (cancel != null)
+            return CANCEL;
+        
+        if (delete != null)
+            return delete();
+        
+        boolean isNew = (doctor.getId() == null);
+        doctor = doctorManager.guardarDoctor(doctor);
+        String key = (isNew) ? "doctor.added" : "doctor.updated";
+        saveMessage(getText(key));
+        
+        if (!isNew)
+            return INPUT;
+        else
+            return SUCCESS;
     }
 }
