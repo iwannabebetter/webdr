3a7817ed083589ca1e138ec17d2af1920b3d3ae6
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-31 17:25:59 +0000

    - Se agregan comentarios. - Se cambian los argumentos del método "obtenerConsultasFecha". - Se cambia el nombre del método "saveConsulta" a "guardar".

--- trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/dao/ConsultaDao.java
+++ trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/dao/ConsultaDao.java
@@ -6,6 +6,7 @@
 package edu.fpuna.dao;
 
 import edu.fpuna.model.Consulta;
+import edu.fpuna.model.User;
 import java.util.Date;
 import java.util.List;
 
@@ -15,15 +16,46 @@ import java.util.List;
  */
 public interface ConsultaDao extends GenericDao<Consulta, Long>  {
 
+    /**
+     * Metodo para obtener las Consultas por idConsulta.
+     * @param id Identificador de la consulta.
+     * @return La consulta con el identificador pasado.
+     */
     public Consulta obtenerConsultaId(Long id) ;
     
+    /**
+     * Metodo para obtener las Consultas de un Pacinete.
+     * @param username Nombre del Paciente.
+     * @return List<Consulta> Lista de Consultas del Paciente.
+     */
     public List<Consulta> obtenerConsultasPaciente(String username);
     
+    /**
+     * Metodo para obtener las Consultas de un Doctor.
+     * @param username Nombre del Doctor.
+     * @return List<Consulta> Lista de Consultas del Doctor.
+     */
     public List<Consulta> obtenerConsultasDoctor(String username);
     
-    public List<Consulta> obtenerConsultasFecha(Date fechaInicio, Date fechaFin);
+    /**
+     * Metodo para obtener las Consultas en una fecha dada.
+     * @param usuario Paciente o Doctor propietario de las consultas a recuperar.
+     * @param fechaInicio Limite inferior del rango de fecha a consultar.
+     * @param fechaFin Limite superior del rango de fecha a consultar.
+     * @return List<Consulta> lista de consulta encontradas.
+     */
+    public List<Consulta> obtenerConsultasFecha(String username, 
+                            Date fechaInicio, Date fechaFin) throws Exception;
 
-    public Consulta saveConsulta(Consulta consulta);
+    /**
+     * Guarda una consulta.
+     * @param consulta La consulta a guardar.
+     */
+    public Consulta guardar(Consulta consulta);
 	  
+    /**
+     * Elimina una consulta.
+     * @param consulta La consulta a eliminar.
+     */
     public void eliminar(Consulta consulta);
 }
