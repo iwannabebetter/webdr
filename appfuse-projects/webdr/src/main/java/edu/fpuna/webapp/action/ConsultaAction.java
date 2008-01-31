3e84050ec9f79868f26c96a6b7e06da4f230aa9e
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-31 18:19:23 +0000

    - Se agregan las propiedades fechas para el listado por fecha. - Se modifican los métodos de listados para la obtención del nombre de usuario. - Actualización de métodos según cambios de clases inferiores (Manager, Dao).

--- appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/ConsultaAction.java
+++ appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/ConsultaAction.java
@@ -21,8 +21,10 @@ public class ConsultaAction extends BaseAction {
 
     private ConsultaManager manager;
     private Consulta consulta;
-    List<Consulta> consultas;
+    private List<Consulta> consultas;
     private Long id;
+    private Date fechaInicio;
+    private Date fechaFin;
 
     public void setConsultaManager(ConsultaManager consultaManager) {
         this.manager = consultaManager;
@@ -33,7 +35,15 @@ public class ConsultaAction extends BaseAction {
     }
 
     public Consulta getConsulta() {
-        return this.consulta;
+        return consulta;
+    }
+    
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+    
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
     }
 
     public void setConsulta(Consulta consulta) {
@@ -42,23 +52,28 @@ public class ConsultaAction extends BaseAction {
         this.consulta = consulta;
     }
 
-    public Consulta getConsulta(Long id) {
-        return manager.getConsulta(id);
-    }
-
-    public String getConsultasPaciente(String username) {
-        consultas = this.manager.getConsultasPaciente(username);
+    public String listConsultasPaciente() {
+        String username = this.getRequest().getRemoteUser();
+        consultas = manager.obtenerConsultasPaciente(username);
         return SUCCESS;
     }
 
-    public String getConsultasDoctor(String username) {
-        consultas = this.manager.getConsultasDoctor(username);
+    public String listConsultasDoctor() {
+        String username = this.getRequest().getRemoteUser();
+        consultas = manager.obtenerConsultasDoctor(username);
         return SUCCESS;
     }
 
-    public String getConsultasFecha(Date fechaInicio, Date fechaFin) {
-        consultas = this.manager.getConsultasFecha(fechaInicio, fechaFin);
-        return SUCCESS;
+    public String listConsultasFecha(Date fechaInicio, Date fechaFin) {
+        try {
+            String username = this.getRequest().getRemoteUser();
+            consultas = manager.obtenerConsultasFecha(username, fechaInicio, fechaFin);
+            return SUCCESS;
+        } catch (Exception ex) {
+            log.fatal(ex);
+            saveMessage(getText("consulta.invalidUserType"));
+            return ERROR;
+        }
     }
     
     public List getConsultas() {
@@ -80,7 +95,7 @@ public class ConsultaAction extends BaseAction {
     public String edit() {
         if (id != null) {
             log.debug("EDITANDO...");
-            consulta = manager.getConsulta(id);
+            consulta = manager.obtenerConsulta(id);
             log.debug("Se obtuvo: " + consulta.getDoctor().getEmail());
         } 
         else {
@@ -99,20 +114,17 @@ public class ConsultaAction extends BaseAction {
 
         boolean isNew = (consulta.getId() == null);
         
-        this.setId(consulta.getId());
-        
         log.debug("GUARDANDO...");
-        log.debug("1.1-)Se obtuvo: " + consulta.getHoraInicio() + "--" +this.getRequest().getParameter("consulta.horaInicio"));
-        log.debug("1.2-)Se obtuvo: " + consulta.getHoraFin());
-        
+        log.debug("1.1-)Se obtuvo: " + consulta.getFechaInicio() + "--" +this.getRequest().getParameter("consulta.fechaInicio"));
+        log.debug("1.2-)Se obtuvo: " + consulta.getFechaFin());
         
         /*
          * Se recupera la consulta modificada debido a que el form (del edit)
          * no envia los datos del Doctor y del Paciente.
          */
-        Consulta oldConsulta = manager.getConsulta(consulta.getId());
-        log.debug("2.1-)Se obtuvo: " + oldConsulta.getHoraInicio());
-        log.debug("2.1-)Se obtuvo: " + oldConsulta.getHoraFin());
+        Consulta oldConsulta = manager.obtenerConsulta(consulta.getId());
+        log.debug("2.1-)Se obtuvo: " + oldConsulta.getFechaInicio());
+        log.debug("2.1-)Se obtuvo: " + oldConsulta.getFechaFin());
         
         // Se actualizan los datos del Doctor y del Paciente.
 
@@ -120,7 +132,7 @@ public class ConsultaAction extends BaseAction {
         consulta.setPaciente(oldConsulta.getPaciente());
 
         // Se guarda la consulta modificada.
-        manager.saveConsulta(consulta);
+        manager.guardarConsulta(consulta);
         
         String key = (isNew) ? "consulta.added" : "consulta.updated";
         saveMessage(getText(key));
