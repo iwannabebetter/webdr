74eced1da0b9a3a3369e2165b7917b7e514bd36f
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-01-31 17:42:42 +0000

    - Se agregan comentarios. - Se cambian nombres de métodos con prefijo "get" por el prefijo "obtener". - Se cambian nombres de métodos con prefijo "save" y "remove" por el prefijo "guardar" y "eliminar", respectivamente.

--- trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/service/ConsultaManager.java
+++ trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/service/ConsultaManager.java
@@ -11,17 +11,52 @@ import java.util.List;
  */
 public interface ConsultaManager extends GenericManager<Consulta, Long> {
 
+    /**
+     * Inicializador del atributo DAO
+     * @param consultaDao
+     */
     public void setConsultaDao(ConsultaDao dao);
 
-    public Consulta getConsulta(Long id);
+    /**
+     * Metodo para la obtener una consulta a travez de su id.
+     * @param id
+     * @return Consulta
+     */
+    public Consulta obtenerConsulta(Long id);
 
-    public List getConsultasPaciente(String username);
+    /**
+     * Metodo para obtener las Consultas de un Paciente.
+     * @param username
+     * @return List lista de las Consultas.
+     */
+    public List<Consulta> obtenerConsultasPaciente(String username);
     
-    public List getConsultasDoctor(String username);
+    /**
+     * Metodo para obtener las Consultas de un Doctor.
+     * @param username
+     * @return List lista de las Consultas
+     */
+    public List<Consulta> obtenerConsultasDoctor(String username);
 
-    public List getConsultasFecha(Date fechaInicio, Date fechaFin);
+    /**
+     * Metodo para obtener las Consultas en una fecha.
+     * @param username Paciente o Doctor propietario de las consultas a recuperar.
+     * @param fechaInicio Limite inferior del rango de fecha a consultar.
+     * @param fechaFin Limite superior del rango de fecha a consultar.
+     * @return List<Consulta> lista de las Consultas.
+     */
+    public List<Consulta> obtenerConsultasFecha(String username, 
+                            Date fechaInicio, Date fechaFin) throws Exception;
 
-    public void saveConsulta(Consulta consulta);
+    /**
+     * Metodo para Guardar una Consulta
+     * @param consulta consulta a guardar
+     */
+    public void guardarConsulta(Consulta consulta);
 
-    public void removeConsulta(long id);
+    /**
+     * Metodo para eliminar una Consulta.
+     * @param id id de Consulta a eliminar.
+     */
+    public void eliminarConsulta(Long id);
 }
