9bb7806676141deb5574690b6cc9f130f7a03654
cdparra <cdparra@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-02-07 20:46:12 +0000

    Implementacion de Interfaz e implementacion del Manager de Reserva

---
+++ trunk/appfuse-projects/webdr/src/main/java/edu/fpuna/service/impl/ReservaManagerImpl.java
@@ -0,0 +1,105 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.fpuna.service.impl;
+
+import edu.fpuna.dao.ReservaDao;
+import edu.fpuna.model.Reserva;
+import edu.fpuna.service.ReservaManager;
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ *
+ * @author Cristhian Parra
+ */
+public class ReservaManagerImpl  extends GenericManagerImpl<Reserva, Long>
+        implements ReservaManager {
+ 
+    // DAO Reserva
+    private ReservaDao reservaDao;
+
+    /**
+     * Constructor
+     * @param reservaDao
+     */
+    public ReservaManagerImpl(ReservaDao reservaDao) {
+        super(reservaDao);
+        this.reservaDao = reservaDao;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setReservaDao(ReservaDao reservaDao) {
+        this.reservaDao = reservaDao;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Reserva obtenerReserva(Long id) {
+        return reservaDao.getReserva(id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<Reserva> obtenerReservas() {
+        return reservaDao.getReservas();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public List<Reserva> obtenerReservasPaciente(String username,boolean pendiente) {
+        if (pendiente) {
+            return reservaDao.getReservasPendientesPac(username);
+        }
+        return reservaDao.getReservasPaciente(username);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public List<Reserva> obtenerReservasDoctor(String username,boolean pendiente) {
+        if (pendiente) {
+            return reservaDao.getReservasPendientesDr(username);
+        }
+        return reservaDao.getReservasDoctor(username);
+    }    
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<Reserva> obtenerReservasFecha(
+                            Timestamp fechaInicio, Timestamp fechaFin) throws Exception {
+        
+        return reservaDao.obtenerReservasFecha(fechaInicio, fechaFin);
+    }    
+
+    /**
+     * {@inheritDoc}
+     */
+    public Reserva guardarReserva(Reserva reserva) {
+        log.debug("Guardando Reserva"+reserva.getId());
+        return reservaDao.guardar(reserva);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void eliminarReserva(Long id) {
+        reservaDao.eliminar(reservaDao.get(id));
+    }
+    
+    /**
+     * Metodo que obtiene todas las Reservas
+     * @return List<Reserva> lista de todas las Reservas
+     */
+    public List<Reserva> getAll() {
+        return reservaDao.getAll();
+    }
+
+}
