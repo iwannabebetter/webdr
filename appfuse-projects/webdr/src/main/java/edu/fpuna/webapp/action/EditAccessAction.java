b5d983484f1ddceb84aee94a68533c979ffb1ac5
ghuttemann <ghuttemann@7d82da66-d73b-0410-a541-41a2a99b7c37>
2008-02-12 23:39:37 +0000

    Action que permite establecer permito de edición sobre objetos/actions

---
+++ appfuse-projects/webdr/src/main/java/edu/fpuna/webapp/action/EditAccessAction.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.fpuna.webapp.action;
+
+import com.opensymphony.xwork2.Preparable;
+import edu.fpuna.Constants;
+
+/**
+ * Action que permite establecer permito de edición
+ * sobre objetos.
+ * @author ghuttemann
+ */
+public class EditAccessAction extends BaseAction implements Preparable {
+    /**
+     * Constante que indica si se permite la edición
+     * de algún objeto por el usuario logueado que
+     * ejecuta el Action.
+     */
+    private Boolean editAccess;
+        
+    public Boolean getEditAccess() {
+        return editAccess;
+    }
+    
+    /*
+     * Por defecto, solo se da permiso de edición
+     * si el usuario está en el rol ADMIN_ROLE.
+     * Si no es este el caso, debemos sobrescribir
+     * este método para sustituir el rol o agregar
+     * otros roles.
+     * Se pueden construir distintas reglas lógicas
+     * sobre los roles en el que debería estar el
+     * usuario logueado. Por ejemplo:
+     *    1. isUserInRole(admin) && isUserInRole(doctor)
+     *    2. isUserInRole(doctor) || isUserInRole(paciente)
+     */
+    protected void setEditAccess() {
+        if (getRequest().isUserInRole(Constants.ADMIN_ROLE))
+            editAccess = true;
+        else
+            editAccess = false;
+    }
+    
+    /*
+     * Por defecto, este método establece el permiso
+     * de edición que tiene el usuario logueado.
+     * Si se debe sobrescribir este método y se desea
+     * tener el mismo comportamiento, se debe agregar,
+     * en la sobrescritura, el código actual del método.
+     */
+    public void prepare() throws Exception {
+        setEditAccess();
+    }
+}
